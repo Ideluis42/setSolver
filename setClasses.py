31db847881954cc82bd3146ce3dac15b49e7c1b9
Ideluis42 <ideluis@olin.edu>
2022-12-12 11:50:24 -0500

    finished sovler

--- setClasses.py
+++ setClasses.py
@@ -10,15 +10,124 @@ class Deck:
     def print_deck(self):
         for i in self.deck:
             print(str(i.number) + i.shape + i.color)
-            
+
+    def remove_card(self, card):
+        self.deck.remove(card)        
 
     def draw_random(self):
-        idx = random.randint(0, len(self.deck))
-        return self.deck.pop(self.deck[idx])
+        idx = random.randint(0, len(self.deck) - 1)
+        return self.deck[idx]
 
     def get_deck_size(self):
         return len(self.deck)
 
+    def find_set(self, card_one, card_two):
+        for i in range(len(self.deck)):
+            card_three = self.deck[i]
+
+            attributes_card_one = {
+                "Number" : card_one.getNumber(),
+                "Color" : card_one.getColor(),
+                "Shape" : card_one.getShape()
+            }
+            
+            attributes_card_two = {
+                "Number" : card_two.getNumber(),
+                "Color" : card_two.getColor(),
+                "Shape" : card_two.getShape()
+            }
+
+            attributes_card_three = {
+                "Number" : card_three.getNumber(),
+                "Color" : card_three.getColor(),
+                "Shape" : card_three.getShape()
+            }
+
+            if (attributes_card_one["Number"] == attributes_card_two["Number"]) and \
+                (attributes_card_two["Number"] == attributes_card_three["Number"]):
+
+                if (attributes_card_one["Color"] != attributes_card_two["Color"]) and \
+                    (attributes_card_two["Color"] != attributes_card_three["Color"]) and \
+                    (attributes_card_one["Color"] != attributes_card_three["Color"]):
+
+                    if (attributes_card_one["Shape"] != attributes_card_two["Shape"]) and \
+                        (attributes_card_two["Shape"] != attributes_card_three["Shape"]) and \
+                        (attributes_card_one["Shape"] != attributes_card_three["Shape"]):
+                        # All same number with different colors and different shapes           
+                        return card_three
+                        
+                    elif attributes_card_one["Shape"] == attributes_card_two["Shape"] and \
+                        attributes_card_two["Shape"] == attributes_card_three["Shape"]:
+                        return card_three
+
+                if (attributes_card_one["Shape"] != attributes_card_two["Shape"]) and \
+                    (attributes_card_two["Shape"] != attributes_card_three["Shape"]) and \
+                    (attributes_card_one["Shape"] != attributes_card_three["Shape"]): 
+                        if attributes_card_one["Color"] == attributes_card_two["Color"] and \
+                            attributes_card_two["Color"] == attributes_card_three["Color"]:
+                            return card_three
+            
+            if attributes_card_one["Color"] == attributes_card_two["Color"] and \
+                attributes_card_two["Color"] == attributes_card_three["Color"]:
+
+                if attributes_card_one["Number"] != attributes_card_two["Number"] and \
+                    attributes_card_two["Number"] != attributes_card_three["Number"] and \
+                    attributes_card_one["Number"] != attributes_card_three["Number"]:
+
+                    if attributes_card_one["Shape"] != attributes_card_two["Shape"] and \
+                        attributes_card_two["Shape"] != attributes_card_three["Shape"] and \
+                        attributes_card_one["Shape"] != attributes_card_three["Shape"]:
+                        return card_three
+                    
+                    elif attributes_card_one["Shape"] == attributes_card_two["Shape"] and \
+                        attributes_card_two["Shape"] == attributes_card_three["Shape"]:
+                        return card_three
+                            
+                if (attributes_card_one["Shape"] != attributes_card_two["Shape"]) and \
+                    (attributes_card_two["Shape"] != attributes_card_three["Shape"]) and \
+                    (attributes_card_one["Shape"] != attributes_card_three["Shape"]):
+                
+                    if attributes_card_one["Number"] == attributes_card_two["Number"] and \
+                        attributes_card_two["Number"] == attributes_card_three["Number"]:
+                        return card_three
+
+            if attributes_card_one["Shape"] == attributes_card_two["Shape"] and \
+                attributes_card_two["Shape"] == attributes_card_three["Shape"]:
+
+                if attributes_card_one["Number"] != attributes_card_two["Number"] and \
+                    attributes_card_two["Number"] != attributes_card_three["Number"] and \
+                    attributes_card_one["Number"] != attributes_card_three["Number"]:
+
+                    if attributes_card_one["Color"] != attributes_card_two["Color"] and \
+                        attributes_card_two["Color"] != attributes_card_three["Color"] and \
+                        attributes_card_one["Color"] != attributes_card_three["Color"]:
+                        return card_three
+
+                    elif attributes_card_one["Color"] == attributes_card_two["Color"] and \
+                        attributes_card_two["Color"] == attributes_card_three["Color"]:
+                        return card_three
+                            
+                if (attributes_card_one["Color"] != attributes_card_two["Color"]) and \
+                    (attributes_card_two["Color"] != attributes_card_three["Color"]) and \
+                    (attributes_card_one["Color"] != attributes_card_three["Color"]):
+                
+                    if attributes_card_one["Number"] == attributes_card_two["Number"] and \
+                        attributes_card_two["Number"] == attributes_card_three["Number"]:
+                        # All same shape with different colors
+                        return card_three
+
+            if (attributes_card_one["Color"] != attributes_card_two["Color"]) and \
+                (attributes_card_two["Color"] != attributes_card_three["Color"]) and \
+                (attributes_card_one["Color"] != attributes_card_three["Color"]) and \
+                attributes_card_one["Number"] != attributes_card_two["Number"] and \
+                attributes_card_two["Number"] != attributes_card_three["Number"] and \
+                attributes_card_one["Number"] != attributes_card_three["Number"] and \
+                (attributes_card_one["Shape"] != attributes_card_two["Shape"]) and \
+                (attributes_card_two["Shape"] != attributes_card_three["Shape"]) and \
+                (attributes_card_one["Shape"] != attributes_card_three["Shape"]):
+                return card_three
+        
+        return None 
 
 class Card:
     def __init__(self, deck, number, shape, color):
@@ -28,8 +137,19 @@ class Card:
 
         deck.add_card(self)
 
+    def getNumber(self):
+        return self.number
+
+    def getShape(self):
+        return self.shape
+
+    def getColor(self):
+        return self.color
+
     def print_card(self):
-        pass
+        print("Number: " + str(self.number))
+        print("Shape: " + self.shape)
+        print("Color: " + self.color)
 
     
 
--- solver.py
+++ solver.py
@@ -2,12 +2,32 @@ import setClasses
 
 deck = setClasses.Deck()
 colors = ["Red", "Green", "Blue"]
-shapes = ["Circle", "Square", "Triangle"]
+shapes = ["Circle", "Diamond", "Triangle"]
 
 for i in colors:
     for k in shapes:
         for j in range(3):
             setClasses.Card(deck, (j + 1), k, i)
 
-print(deck.get_deck_size())
-deck.print_deck()
+
+for i in range(deck.get_deck_size()//3):
+    card_one = deck.draw_random()
+    deck.remove_card(card_one)
+    card_two = deck.draw_random()
+    deck.remove_card(card_two)
+
+    card_three = deck.find_set(card_one, card_two)
+    
+    if card_three != None:
+        deck.remove_card(card_three)
+        print("SET: ")
+        card_one.print_card()
+        print("---------")
+        card_two.print_card()
+        print("---------")
+        card_three.print_card()
+        print("---------")
+
+
+
+
